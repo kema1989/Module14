cb7013dedb2aaefd29f751c28d20da4df37c8c95
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-10-26 22:48:37 +0600

    Добавьте файлы проекта.

---
+++ M14U1-1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace M14U1_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Добавим Россию с её городами
+            var russianCities = new List<City>();
+            russianCities.Add(new City("Москва", 11900000));
+            russianCities.Add(new City("Санкт-Петербург", 4991000));
+            russianCities.Add(new City("Волгоград", 1099000));
+            russianCities.Add(new City("Казань", 1169000));
+            russianCities.Add(new City("Севастополь", 449138));
+
+            //var longCities = from longCity in russianCities
+            //                 where longCity.Name.Length < 10
+            //                 orderby longCity.Name
+            //                 select longCity;
+
+            //foreach(var longCity in longCities)
+            //    Console.WriteLine(longCity);
+            var longCities = russianCities.Where(city => city.Name.Length < 10).OrderBy(city => city.Name.Length);
+        }
+
+        // Создадим модель класс для города
+        public class City
+        {
+            public City(string name, long population)
+            {
+                Name = name;
+                Population = population;
+            }
+
+            public string Name { get; set; }
+            public long Population { get; set; }
+        }
+    }
+}
---
+++ Module14/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Module14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var objects = new List<object>()
+            {
+                1,
+                "Сергей",
+                "Андрей",
+                300,
+            };
+
+            var result = from p in objects
+                         where p is string
+                         orderby p
+                         select p;
+            
+            foreach(var name in result)
+            {
+                Console.WriteLine(name);
+            }
+
+            foreach (var name in objects.Where(p => p is string).OrderBy(p => p))
+                Console.WriteLine(name);
+
+        }
+    }
+}
