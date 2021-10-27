506fe72a729ad75f03bc762acb9d0dc76046334f
kema1989 <86516029+kema1989@users.noreply.github.com>
2021-10-27 23:37:50 +0600

    14.2.1 & 14.2.3 ✔

---
+++ M14U2-1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace M14U2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "Обезьяна", "Лягушка", "Кот", "Собака", "Черепаха" };
+
+            var newWords = words.Select(s => new { Name = s, Length = s.Length }).OrderBy(s => s.Length);
+
+            foreach(var word in newWords)
+                Console.WriteLine(word.Name);
+        }
+    }
+}
---
+++ M14U2-3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace M14U2_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new List<Student>()
+            {
+                new Student{Name = "Андрей", Age = 23, Languages = new List<string>{"английский", "немецкий"} },
+                new Student{Name = "Сергей", Age = 27, Languages = new List<string>{"английский", "французский"}},
+                new Student{Name = "Дмитрий", Age = 29, Languages = new List<string>{"английский", "испанский"}},
+                new Student{Name = "Василий", Age = 24, Languages = new List<string>{"испанский", "немецкий"}}
+            };
+
+            var choosed = from s in students
+                          where s.Age < 27
+                          let year = DateTime.Today.Year - s.Age
+                          select new Application { Name = s.Name, YearOfBirth = year };
+
+            foreach(var student in choosed)
+                Console.WriteLine($"{student.Name} {student.YearOfBirth} года рождения");
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public List<string> Languages { get; set; }
+    }
+    class Application
+    {
+        public string Name { get; set; }
+
+        public int YearOfBirth { get; set; }
+    }
+}
